c1e53d1e750eb6844ac7a9bfe3ba6af063ef6f45
jolly <jolly@88tech.net>
2024-05-06 10:45:44 +0800

    change code

--- App.tsx
+++ App.tsx
@@ -1,20 +1,11 @@
 import React from 'react';
 import {SafeAreaView} from 'react-native';
-import ProductTable from './src/02_Component/ProductTable';
-
-const PRODUCTS = [
-  {category: '水果', price: '￥1', name: 'PingGuo'},
-  {category: '水果', price: '￥1', name: 'HuoLongGuo'},
-  {category: '水果', price: '￥2', name: 'BaiXiangGuo'},
-  {category: '蔬菜', price: '￥2', name: 'BoCai'},
-  {category: '蔬菜', price: '￥4', name: 'NanGua'},
-  {category: '蔬菜', price: '￥1', name: 'WanDou'},
-];
+import App02 from './src/02_Component/App02';
 
 export default function App() {
   return (
     <SafeAreaView style={{marginHorizontal: 30}}>
-      <ProductTable products={PRODUCTS} />
+      <App02/>
     </SafeAreaView>
   );
 }
